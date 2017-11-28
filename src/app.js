7cb1038f6489662ac9f3c32d54e4917440e5f9de
Fran Bosquet <fran@redradix.com>
2017-11-28 11:18:15 +0100

    Dev tols working

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 import Greeting from './components/greeting'
 
 const App = () => <Greeting title="Hello React"/>
 
+const reducer = (state = { any: 'hola' }, action) => {
+  switch(action.type){
+    default: return state
+  }
+}
+
+let store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('app')
 )
