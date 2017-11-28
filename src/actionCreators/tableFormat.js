f3825994722842c1bfc7419e8aeda557a38ab727
Fran Bosquet <fran@redradix.com>
2017-11-28 16:28:11 +0100

    Format table action creator refactor

--- src/actionCreators/tableFormat.js
+++ src/actionCreators/tableFormat.js
@@ -1,2 +1,27 @@
-import { ADD_FORMAT } from '../actionTypes'
+import { SET_FORMAT } from '../actionTypes'
+import { selectCells } from './tableSelection'
+import { fromRangeToCells, populateWith } from '../utils'
+
+const addFormat = (cells, format) => {
+  return {
+    type: SET_FORMAT,
+    cells
+  }
+}
+
+export const formatCells = (format, selection) => {
+  return (dispatch, getState) => {
+    // TODO: Remove this
+    const selected = fromRangeToCells({
+      fromRow: selection[0],
+      fromColumn: selection[1],
+      toRow: selection[2],
+      toColumn: selection[3]
+    })
+    const cells = populateWith(selected, format) 
+    console.log('He llegado', selected)
+    console.log('He llegado', cells)
+    dispatch(addFormat(cells, 'red'))
+  }
+}
 
