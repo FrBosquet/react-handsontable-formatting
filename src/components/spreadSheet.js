b851549741d5448c3e486663bd4ae0c2942c2036
Fran Bosquet <fran@redradix.com>
2017-11-28 17:39:56 +0100

    Working basic format edition

--- src/components/spreadSheet.js
+++ src/components/spreadSheet.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
 import HotTable from 'react-handsontable'
 import * as HoT from 'handsontable'
+import _ from 'lodash'
 
 function renderer(instance, td, row, col, prop, value, cellProperties){
-  HoT.renderers.TextRenderer.apply(this, arguments)
-  //if (value.style) td.style = _.merge({}, td.style, value.style)
+  const args = Array.prototype.slice.apply(arguments)
+  args[5] = value.content
+  HoT.renderers.TextRenderer.apply(this, args)
+  if (value.style) {
+    // debugger
+    Object.entries(value.style).map(([k, v]) => td.style[k] = v)
+    console.log('aSDFASDFG')
+  }
   //td.textContent = value
 }
 
@@ -12,7 +19,7 @@ class SpreadSheet extends Component {
   constructor(props) {
     super(props)
     this.handleFormatCells_1 = this.handleFormatCells.bind(this, { color: 'red' })
-    this.handleFormatCells_2 = this.handleFormatCells.bind(this, { fontSize: 'bold' })
+    this.handleFormatCells_2 = this.handleFormatCells.bind(this, { fontWeight: 'bold' })
   }
   handleFormatCells(format, e) {
     e.preventDefault()
@@ -23,12 +30,12 @@ class SpreadSheet extends Component {
   render() {
     const { data, handleSelectCells, handleFormatCells } = this.props
     console.log('render')
-    console.log(this.el)
+    console.log(this.HoT)
     return (
       <div className='table-wrapper'>
         <button id='paco' onClick={this.handleFormatCells_1}>rojo</button>
         <button id='paco' onClick={this.handleFormatCells_2}>negrita</button>
-        <HotTable ref={el => this.HoT = el.hotInstance}
+        <HotTable ref={el => this.HoT = el && el.hotInstance}
           data={data}
           outsideClickDeselects={false}
           renderer={renderer}
--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,3 +1,24 @@
+export const getFormat = state => state.tableFormat
+export const getCells = state => state.tableContent
 export const getFormatedCells = state => {
-  return state.tableContent
+  const format = getFormat(state)
+  const cells = getCells(state)
+  const formatedCells = cells.map((row, i) => row.map((cell, j)=>{
+    return format[i] && format[i][j] ?
+    { content: cell, style: format[i][j]} :
+    { content: cell}
+  }))
+  console.log('EOHHH')
+  const tmp = Object.keys(format).reduce((acc, rowIndex) => {
+    const row = format[rowIndex]
+    acc[rowIndex] = Object.keys(row).reduce((acc2, columnIndex) => {
+      const column = row[columnIndex]
+      acc2[columnIndex] = { style: { $set: column } }
+      return acc2
+    }, {})
+    return acc
+  }, {})
+  console.log(tmp)
+
+  return formatedCells
 }
