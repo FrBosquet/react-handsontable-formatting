f83eadcfea04456fe9f675d0f1161099f2e8d454
Fran Bosquet <fran@redradix.com>
2017-11-29 15:34:13 +0100

    :art:

--- src/actionCreators/tableFormat.js
+++ src/actionCreators/tableFormat.js
@@ -2,18 +2,10 @@ import { SET_FORMAT } from '../actionTypes'
 import { selectCells } from './tableSelection'
 import { fromRangeToCells, populateWith } from '../utils'
 
-const addFormat = (selected, format) => {
+export const formatCells = (selected, format) => {
   return {
     type: SET_FORMAT,
-    payload: { selected, format }
-  }
-}
-
-export const formatCells = ( selection, format ) => {
-  return (dispatch, getState) => {
-    const selected = fromRangeToCells( selection )
-    //const cells = populateWith(selected, format)
-    dispatch(addFormat( selected, format))
+    payload: { selected: fromRangeToCells(selected), format }
   }
 }
 
