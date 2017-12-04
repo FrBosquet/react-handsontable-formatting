29d59bb7ec009578108587c593bc07be2f503e99
Fran Bosquet <fran@redradix.com>
2017-12-04 12:26:42 +0100

    Add memoized selectors

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,4 +1,14 @@
-export const getCells = state => state.tableContent
+import { createSelector } from 'reselect'
+
+export const getCellsData = state => state.tableContent
 export const getTableConfig = state => state.tableConfig
 export const getShowColHeaders = state => getTableConfig(state).showColHeaders
 export const getShowRowHeaders = state => getTableConfig(state).showRowHeaders
+export const getCellsContent = createSelector(
+  getCellsData,
+  data => data.map( row => row.map( cell => cell.content))
+)
+export const getCellsStyle = createSelector(
+  getCellsData,
+  data => data.map( row => row.map( cell => cell.Style || {}))
+)
