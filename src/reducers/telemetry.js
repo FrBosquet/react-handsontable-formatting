efdde6ed16bb22999f40dfc6befaff3d1ac7f4a8
Fran Bosquet <fran@redradix.com>
2017-12-04 14:13:52 +0100

    Telemetry reducer

---
+++ src/reducers/telemetry.js
@@ -0,0 +1,17 @@
+import * as actionTypes from '../actionTypes'
+
+const mock = {
+  value1: 'njksbnkfjs',
+  value2: 'jnsjfkbnfshnbfds',
+  something: 'sjhjkhksfds',
+  satellite: 'adsffsd',
+  delta: 'njfdnjlsdf' 
+}
+
+const reducer = (state = mock, action) => {
+  switch (action.type) {
+    default: return state
+  }
+}
+
+export default reducer
